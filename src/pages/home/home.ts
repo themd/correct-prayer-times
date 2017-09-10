c9bb8d1b781175ca4bfbdc9d6c06c21039d1dfe2
Elvis Dedic <elvis26112009@gmail.com>
2017-09-10 19:27:32 +0200

    logic for home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PrayerTimes } from '../../services/prayer-times.service'
+import { Subscription } from "rxjs/Rx"
+
+interface PrayerDay {
+  Asr: string
+  Dhuhr:string  
+  Fajr:string  
+  Imsaak:string
+  Isha:string  
+  Maghrib:string
+  Sunrise:string
+  Sunset:string
+}
 
 @Component({
   selector: 'page-home',
@@ -7,8 +20,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  prayerTimesSubscription$: Subscription
 
+  month: object
+  today: PrayerDay
+  currentDate: Date
+
+  constructor(public navCtrl: NavController, private prayerTimesService: PrayerTimes) {
+    this.currentDate = new Date()
+    console.log(this.currentDate.getDate())
+    this.prayerTimesSubscription$ = this.prayerTimesService.prayerTimeList$.subscribe(month=> {
+      this.month = month
+      this.today = this.month[`${this.currentDate.getDate()}`]
+      console.log(this.today.Fajr)   
+    })
+  }
+
+  ionViewDidLoad() {    
+    this.fetchMonth()
+  }
+
+  fetchMonth() {
+    this.prayerTimesService.fetchPrayerTimes()
   }
 
 }
