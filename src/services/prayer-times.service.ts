82a1eba246f9922810df1a00a131c59ef17d7f33
themd <mrad.mohammad@gmail.com>
2019-07-27 05:58:11 +0300

    Update prayer-times.service.ts
    for ionic 4

--- src/services/prayer-times.service.ts
+++ src/services/prayer-times.service.ts
@@ -1,20 +1,23 @@
-import { Injectable } from '@angular/core'
-import { Http } from '@angular/http'
-import 'rxjs/add/operator/map'
-import { Observable, Subject } from "rxjs/Rx"
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { Subject, Observable } from 'rxjs';
+import 'rxjs/add/operator/take';
 
-const PRAYER_URL = 'http://praytime.info/getprayertimes.php?lat=47.070714&lon=15.439503999999943&gmt=120&m=9&y=2017&school=6&params=14.6,4.0,14.6,0,0,0,0,1,18.0,0'
-const CORS = 'https://cors-anywhere.herokuapp.com/'
+const PRAYER_URL = 'http://praytime.info/getprayertimes.php?lat=47.070714&lon=15.439503999999943&gmt=120&m=9&y=2017&school=6&params=14.6,4.0,14.6,0,0,0,0,1,18.0,0';
+const CORS = 'https://cors-anywhere.herokuapp.com/';
 
 @Injectable()
 export class PrayerTimes {
-
-    private readonly _prayerTimeSubject$: Subject<any> = new Subject()
-    prayerTimeList$: Observable<any> = this._prayerTimeSubject$.asObservable()
-    
-    constructor(private http: Http) {}
-
-    fetchPrayerTimes(longitude?: string, latitude?: string, month?: number) {
-        this.http.get(CORS+PRAYER_URL).take(1).subscribe(res=>this._prayerTimeSubject$.next(res.json()))
-    }
+  private readonly prayerTimeSubject$: Subject<any> = new Subject();
+  prayerTimeList$: Observable<any> = this.prayerTimeSubject$.asObservable();
+  constructor(private http: HttpClient) { }
+  fetchPrayerTimes(longitude?: string, latitude?: string, month?: number) {
+    return this.http.get(CORS + PRAYER_URL)
+      .take(1).subscribe(res => {
+        this.prayerTimeSubject$.next(res
+        );
+        console.log(res);
+      });
+  }
 }
